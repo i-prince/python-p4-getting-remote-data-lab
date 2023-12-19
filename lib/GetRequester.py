d3fcb5743ab20a55ae6333e2c631ab76bdd8d2d8
i-prince <molatonny@gmail.com>
2023-12-19 18:19:35 +0300

    completed Assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,18 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        # Send a GET request to the URL and return the response body
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        # Use get_response_body to get the response body
+        response_body = self.get_response_body()
+
+        # Convert the response body to a Python list or dictionary
+        try:
+            json_data = json.loads(response_body)
+            return json_data
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            return None
